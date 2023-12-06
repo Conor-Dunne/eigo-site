0f2bad99ded42fd02842298910364af64a356973
Conor Dunne <conordunne@outlook.ie>
2023-12-07 07:28:47 +0900

    Test

--- app/[lang]/dbposts/[slug]/page.jsx
+++ app/[lang]/dbposts/[slug]/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import VocabSideMenu from "../../components/vocab-menu";
 import ContentWithPopups from "../components/ContentWithPopups"
 import { promises as fs } from "fs";
+import jmdictData from "@/data/jmdict-eng-common-3.5.0.json"
 
 
 
@@ -28,13 +29,13 @@ const SinglePage = async ({ params }) => {
 
   const data = await getData(slug);
 
-  const file = await fs.readFile(
-    process.cwd() + "/data/jmdict-eng-common-3.5.0.json",
-    "utf8"
-  );
-  const jmdict = JSON.parse(file);
+  // // const file = await fs.readFile(
+  // //   process.cwd() + "/data/jmdict-eng-common-3.5.0.json",
+  // //   "utf8"
+  // // );
+  // const jmdict = JSON.parse(jmdictData);
   
-  console.log(jmdict.words[0].kanji[0].text)
+  console.log(jmdictData.words[0].kanji[0].text)
 
   return (
     <main className="prose prose-slate text-lg mx-auto mt-10 md:mt-20 px-6 pb-28">
@@ -46,7 +47,7 @@ const SinglePage = async ({ params }) => {
       <article className="mt-8">
         <Image src={data.img} width={600} height={600} alt="Picture of the author" className="rounded-lg" />
         {/* <ContentWithPopups content={data.desc} words={data.keyWords} /> */}
-        <h1>{jmdict.words[0].kanji[0].text}</h1>
+        <h1>{jmdictData.words[0].kanji[0].text}</h1>
       </article>
     </main>
   );
