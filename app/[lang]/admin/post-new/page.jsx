9700fe13f3df0753eb36503fff54a0046dd09346
Conor Dunne <conordunne@outlook.ie>
2024-02-14 05:02:55 +0000

    Update

--- app/[lang]/admin/post-new/page.jsx
+++ app/[lang]/admin/post-new/page.jsx
@@ -1,10 +1,17 @@
 "use client"
 
 import { useState } from "react"
+import Image from "next/image";
+
 
 export default function CreatPost () {
 
   const [isAdmin, setIsAdmin] = useState(true)
+  const [imgSrc, setImgSrc] = useState(null);
+
+  const handleInputChangeImg = (e) => {
+    setImgSrc(e.target.value);
+  };
 
 
 
@@ -17,7 +24,7 @@ export default function CreatPost () {
       <div className="w-full m-auto flex my-4">
         <div className="flex flex-col justify-center items-center m-auto">
           <p className="text-2xl text-slate-200 font-bold p-3">
-            Add A Wonderful Blog 🚀
+            Make it wonderful! 🚀
           </p>
           <form onSubmit={() => alert("test")}>
             <input
@@ -26,6 +33,14 @@ export default function CreatPost () {
               type="text"
               className="rounded-md px-4 w-full py-2 my-2 "
             />
+            <input
+              // ref={titleRef}
+              placeholder="Add image source url"
+              onChange={handleInputChangeImg}
+              type="text"
+              className="rounded-md px-4 w-full py-2 my-2 "
+            />
+            {imgSrc ? <Image src={imgSrc} width={200} height={200} alt="Picture of the author" className="rounded-lg" priority={true} /> : <p>no image</p>}
             <textarea
               // ref={descriptionRef}
               placeholder="Enter Description"
