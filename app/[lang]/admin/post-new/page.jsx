179ab1cf1086da1900b1f626b67c88143b3a15b7
Conor Dunne <conordunne@outlook.ie>
2024-03-03 22:42:56 +0000

    Add state var for vocab

--- app/[lang]/admin/post-new/page.jsx
+++ app/[lang]/admin/post-new/page.jsx
@@ -31,6 +31,7 @@ export default function CreatPost() {
   const [imgSrc, setImgSrc] = useState("https://images.unsplash.com/photo-1707343848723-bd87dea7b118?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   const [slug, setSlug] = useState("asdasd");
   const [desc, setDesc] = useState("Test desc");
+  const [vocab, setVocab] = useState([]);
   
 
   const router = useRouter();
