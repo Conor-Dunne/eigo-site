27e60fb7a5030e937eddacff97815ed6c2207c6c
Conor Dunne <conordunne@outlook.ie>
2024-05-08 07:40:16 +0900

    Design changes

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -34,7 +34,7 @@ export default async function Home({
 
   return (
     <>
-      <div
+      {/* <div
         id="hero"
         className="relative flex justify-center items-center text-white w-full h-4 px-6 py-14 mb-1 -z-10 "
       >
@@ -53,14 +53,14 @@ export default async function Home({
             loading="lazy"
           />
         </div>
-      </div>
+      </div> */}
       <div>
-        <div className="flex justify-center py-10">
+        {/* <div className="flex justify-center py-10">
           <h2 className=" text-md font-light text-xs md:text-sm text-slate-500 text-center">
             {page.home.description}
           </h2>
-        </div>
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 px-3 xl:px-40">
+        </div> */}
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 px-3 py-4 md:py-24  xl:px-40">
           <div className="col-span-1 md:col-span-2 md:row-span-2">
             {" "}
             <Link
