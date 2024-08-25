2b7607b49fc3967b5ddc9f56deaab5d0aabd9142
Conor Dunne <conordunne@outlook.ie>
2024-08-25 18:18:45 +0900

    Update TS

--- app/[lang]/dbposts/loading.tsx
+++ app/[lang]/dbposts/loading.tsx
@@ -4,13 +4,11 @@ import { ThreeCircles } from 'react-loader-spinner'
 ;
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
     return (
       <div className='flex justify-center items-center h-screen'>
         <ThreeCircles
     height="80"
     width="80"
-    radius="9"
     color="#21acb8"
     ariaLabel="three-dots-loading"
   />
